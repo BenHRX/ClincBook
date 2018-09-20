1e2f0625b0fd9251b1bbac62cd11856f0d40ac5f
Ben Huang <rx9702@163.com>
2018-09-21 04:28:43 +0800

    暂存版本
    更新了服务器数据获取，但是业务逻辑有问题。
    Update program to fetch data from server, but it has a big misunderstand for the business logic.

--- pages/bookPage/ClinicBooking.js
+++ pages/bookPage/ClinicBooking.js
@@ -1,4 +1,5 @@
-var serverCities = ["深圳","北京","上海","广州"];
+var serverCities = ["深圳", "北京", "上海", "广州"];
+var app_globaldata = getApp().globalData;
 
 // pages/bookPage/ClinicBooking.js
 Page({
@@ -156,6 +157,7 @@ Page({
       return (new Date(today + days * 24 * 60 * 60 * 1000).getDate())
     }
   },
+
   calcFullDate: function(days) { // 只是计算自增的
     var choseDate = new Date((new Date().valueOf()) + days * 24 * 60 * 60 * 1000);
     var choseDateArray = [];
@@ -164,13 +166,47 @@ Page({
     choseDateArray['year'] = choseDate.getFullYear();
     return choseDateArray;
   },
+
   setNumFormat: function(n) {
     n = n.toString();
     return n[1] ? n : '0' + n;
   },
+
   // 读取可用服务
   serviceLoad: function() {
-    var services = ['内科及全科', '妇产科', '儿科', '儿童保健', '齿科', '齿科正畸', '眼科', '耳鼻喉科', '皮肤科', '营养科', '高端体检'];
+    var that = this;
+    wx.request({
+      // url: 'http://localhost/ControlSystem/index.php/hospital_controller/response_detail_by_city/' + this.data.CityChosen,
+      url: app_globaldata.g_base_api_url + 'hospital_controller/response_detail_by_city',
+
+      //如果小程序设置 method:’POST’，PHP后台$_POST无法获取前端传参
+      // 小程序：不设置 method或设置用GET,小程序默认用’GET’, 后台通过$_GET可以获取参数
+      // 小程序: 设置用'POST', 修改content-type 为application/x-www-form-urlencoded
+      data: {
+        'city': this.data.CityChosen
+      },
+      method: 'POST',
+      // method: 'GET',  // Get is ok
+      header: {
+        // 'content-type': 'json'
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function(res) {
+        that.processDetailDataInCity(res);
+      }
+    });
+  },
+
+  processDetailDataInCity: function(res) { // This should include the hospital and the department.
+    console.log(res.data);
+    // Control the services can provided in the city.
+    var services = [];
+    for (var idx in res.data) {
+      if (services.indexOf(res.data[idx]['depart']) == -1) {
+        services.push(res.data[idx]['depart']);
+      }
+    }
+    console.log(services);
     var tmp_list = [];
     var tmp_full_list = [];
 
@@ -186,11 +222,27 @@ Page({
     for (var x in tmp_full_list) {
       console.log("List:" + tmp_full_list[x]);
     }
+
+    // Fetch the hospital list in this city
+    var tmpServerData = [];
+    for (var idy in res.data) {
+      if (tmpServerData.filter(element => element['h_Title'] == res.data[idy]['name']).length == 0) {
+        var tmp_hospital_detail = {
+          "h_img": app_globaldata.g_base_image_url + res.data[idy]['view_photo'],
+          "h_Title": res.data[idy]['name'],
+          "h_Summary": res.data[idy]['address'] + "\n" + res.data[idy]['hospital_summary'],
+        };
+        tmpServerData.push(tmp_hospital_detail);
+      }
+    }
+
     this.setData({
       department_list: tmp_full_list,
       department_chose: services[0],
+      HospitalData: tmpServerData,
     });
   },
+
   // 对应department click
   onClickDepartment: function(event) {
     var data_click = event.currentTarget.dataset.click;
@@ -201,36 +253,39 @@ Page({
       department_idx: dep_idx,
     });
   },
+
   // 医院数据读取
   loadHospitalData: function() {
-    var tmpServerData = [{
-        "h_img": "../../image/TestUsing/TestView01.png",
-        "h_Title": "广州医科大学附属第一医院",
-        "h_Summary": "广州市沿江路151号(原市总工对面)\n电话：020-83062114",
-      },
-      {
-        "h_img": "../../image/TestUsing/TestView02.png",
-        "h_Title": "广州医科大学附属第二医院",
-        "h_Summary": "广州市海珠区昌岗东路250号\n咨询电话：86-020-34152282",
-      },
-      {
-        "h_img": "../../image/TestUsing/TestView03.png",
-        "h_Title": "广州医科大学附属第三医院",
-        "h_Summary": "广州市荔湾区多宝路63号\n邮编：510150",
-      }
-    ];
-    this.setData({
-      HospitalData: tmpServerData,
-    });
+    // var tmpServerData = [{
+    //     "h_img": "../../image/TestUsing/TestView01.png",
+    //     "h_Title": "广州医科大学附属第一医院",
+    //     "h_Summary": "广州市沿江路151号(原市总工对面)\n电话：020-83062114",
+    //   },
+    //   {
+    //     "h_img": "../../image/TestUsing/TestView02.png",
+    //     "h_Title": "广州医科大学附属第二医院",
+    //     "h_Summary": "广州市海珠区昌岗东路250号\n咨询电话：86-020-34152282",
+    //   },
+    //   {
+    //     "h_img": "../../image/TestUsing/TestView03.png",
+    //     "h_Title": "广州医科大学附属第三医院",
+    //     "h_Summary": "广州市荔湾区多宝路63号\n邮编：510150",
+    //   }
+    // ];
+    // this.setData({
+    //   HospitalData: tmpServerData,
+    // });
   },
-  onClickHospital: function(event){
+
+  onClickHospital: function(event) {
     var click = event.currentTarget.dataset.idx;
     console.log("Click Hospital " + click);
     wx.navigateTo({
       url: '../DoctorView/DoctorView?hospital=' + this.data.HospitalData[click]['h_Title'] + "&department=" + this.data.department_chose + "&date=" + this.data.choseDate + "&city=" + this.data.CityChosen + "&search=0",
     });
   },
-  onHosipitalPosition: function(event){
+
+  onHosipitalPosition: function(event) {
     var click = event.currentTarget.dataset.idx;
     console.log("Click Hospital position mark " + click);
     wx.navigateTo({
@@ -239,22 +294,44 @@ Page({
   },
 
   /* 显示目前选择的城市 */
-  bindPickerChange: function(event){
+  bindPickerChange: function(event) {
     console.log(event);
     var chosenIndex = event.detail.value;
     console.log(this.data.cityArray[chosenIndex]);
     this.setData({
       CityChosen: this.data.cityArray[chosenIndex],
     });
+    this.serviceLoad();
   },
 
   /* 获得地区信息 */
-  loadServerCities: function(){
+  processCityData: function(res) {
+    var city_list = [];
+    for (var idx in res.data) {
+      city_list.push(res.data[idx]['name']);
+    }
+    console.log(city_list);
     this.setData({
-      cityArray: serverCities,
+      cityArray: city_list,
     });
   },
-  onTapSearch: function(event){
+  loadServerCities: function() {
+    // this.getServerCity();
+    var that = this;
+    wx.request({
+      url: app_globaldata.g_base_api_url + 'getCityArray',
+      method: 'GET',
+      header: {
+        'content-type': 'json'
+      },
+      success: function(res) {
+        that.processCityData(res);
+      }
+    });
+  },
+
+  // 直接单击Search
+  onTapSearch: function(event) {
     wx.navigateTo({
       url: "../DoctorView/DoctorView?hospital=Nil&department=Nil&date=Nil&city=" + this.data.CityChosen + "&search=search_doctor",
     });
